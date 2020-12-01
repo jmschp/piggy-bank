4f69c7f13f56d15b5decea41f5b11d26c34a2142
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-12-01 17:12:08 -0300

    Configured email senders for development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "http://kids-piggy-bank.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "piggy_bank_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "kids-piggy-bank.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
