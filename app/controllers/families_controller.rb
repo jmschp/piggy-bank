6b374867acd348d27f3dbba73d3af98f8e2ea34f
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-12-02 14:49:56 -0300

    Deleted unecessary code

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,27 +1,4 @@
 class FamiliesController < ApplicationController
-  def index
-    user = current_user
-    user_family_id = user.family_id
-    @my_family = User.where(family_id: user_family_id)
-  end
-
-  def new
-    @family = Family.new
-  end
-
-  def create
-    @user = current_user
-    @family = Family.new(family_params)
-
-    if @family.save
-      @user.admin = true
-      @user.family = @family
-      @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
 
   private
 
