ad2842644e6c169fe008fc4df0e2374a337102e5
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-12-02 19:34:49 -0300

    kids card, family index page, seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+families = %w[Pimenta Hargreaves Buscapé]
+task_type = ["Casa", "Escola"]
+
+families.each do |family|
+  new_fam = Family.create!(name: family)
+  User.create!(
+    name: "Leader #{family}",
+    email: "#{family}@piggy.com",
+    password: 123123,
+    admin: true,
+    family_id: new_fam.id
+  )
+end
+
+filhos = []
+10.times do
+  filho = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 123123,
+    admin: false,
+    family_id: Family.all.sample.id
+  )
+  filhos << filho
+end
+
+
+50.times do
+  Task.create!(
+    title: Faker::Lorem.paragraph,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    points: rand(1..5),
+    task_type: task_type.sample,
+    user_id: filhos.sample.id
+  )
+end
