59ee8f134a660d2a73ce7706daac5dacc54a4980
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-12-06 20:06:05 -0300

    Implemented chatroom

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,13 +1,19 @@
 class Family < ApplicationRecord
   has_many :users
+  has_one :chatroom
 
   validates :name, presence: true, length: { maximum: 150 }
 
   # after_commit :set_family_leader, only: :create
+  after_commit :create_family_chatroom, only: :create
 
   private
 
   def set_family_leader
     self.users.first.update(admin: true)
+  end
+
+  def create_family_chatroom
+    Chatroom.create(name: "Sala da Família #{self.name}", family_id: self.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_182039) do
+ActiveRecord::Schema.define(version: 2020_12_06_212446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2020_12_04_182039) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["family_id"], name: "index_chatrooms_on_family_id"
+  end
+
   create_table "families", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -50,6 +58,16 @@ ActiveRecord::Schema.define(version: 2020_12_04_182039) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "punishments", force: :cascade do |t|
@@ -104,7 +122,10 @@ ActiveRecord::Schema.define(version: 2020_12_04_182039) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "chatrooms", "families"
   add_foreign_key "goals", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "punishments", "users"
   add_foreign_key "tasks", "users"
   add_foreign_key "users", "families"
