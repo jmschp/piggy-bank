92fed320fd3fd6b19c2438e3d6e4865914feb880
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-12-07 17:30:18 -0300

    Added field to goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,13 @@
 class Goal < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { maximum: 250 }
+  validates :total_points, presence: true
+
+  after_commit :finish_goal?, only: :update
+
+  def finish_goal
+    if self.points == self.total_points
+      self.update(finished: true)
+    end
+  end
 end
