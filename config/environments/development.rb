df960468e680274fc323de464e983f50d11eca89
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-12-10 22:38:31 -0300

    Configed pwa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,4 +66,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow requests by adding the following to your environment configuration
+  config.hosts << /\A[a-z0-9]+\.ngrok\.io/
 end
